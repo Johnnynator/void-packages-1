fd5ab7fc63ab1916bde540710d4eae2ee2713547
John <johnz@posteo.net>
2019-11-20 18:42:47 +0100

    travis/fetch_upstream.sh: prefer git over chroot-git

--- common/travis/fetch_upstream.sh
+++ common/travis/fetch_upstream.sh
@@ -2,11 +2,12 @@
 #
 # changed_templates.sh
 
-if command -v chroot-git >/dev/null 2>&1; then
-	GIT_CMD=$(command -v chroot-git)
-elif command -v git >/dev/null 2>&1; then
+if command -v git >/dev/null 2>&1; then
 	GIT_CMD=$(command -v git)
+elif command -v chroot-git >/dev/null 2>&1; then
+	GIT_CMD=$(command -v chroot-git)
 fi
+# We can't use chroot-get for fetching anything (?)
 
 /bin/echo -e '\x1b[32mFetching upstream...\x1b[0m'
 $GIT_CMD fetch --depth 200 git://github.com/void-linux/void-packages.git master
